51b65b72d09fd0bb890501058e8c6cf6d061b971
matentzn <nicolas.matentzoglu@gmail.com>
2018-10-03 18:25:09 +0100

    First release
    Had to deactive equivalence checking, no labels violations and whitespace in definitions checking.

--- src/ontology/wb_reset.sh
+++ src/ontology/wb_reset.sh
@@ -15,6 +15,7 @@ wget -N https://raw.githubusercontent.com/obophenotype/upheno/master/wbphenotype
 $ROBOT convert --input wb_reset/wbphenotype_orig.obo -o wb_reset/wbphenotype_orig.owl
 # 3. Merge the old ontologies into the edit file
 $ROBOT merge --collapse-import-closure false --include-annotations true --input wbphenotype-edit.owl --input wb_reset/wbphenotype_orig.owl --input wb_reset/wbphenotype_eq.owl --output wb_reset/wbphenotype-edit.ofn
-# 4. Compare the two edit file to make sure nothing is lost (diff should be empty)
+# Replace hasComponent (which is used everwhere to hang off the abnormal modifier by 'has modifier')
+sed -i '' 's/RO_0002180/RO_0002573/g' wb_reset/wbphenotype-edit.owl
 mv wb_reset/wbphenotype-edit.ofn wb_reset/wbphenotype-edit.owl
 diff -u wbphenotype-edit.owl wb_reset/wbphenotype-edit.owl > wb_reset/diff.txt
